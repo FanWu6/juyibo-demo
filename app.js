b65e6223f9fc1ce31bf472d210f844d13abcc2f6
IMfeather <309326243@qq.com>
2017-01-12 03:29:06 +0800

    接入野狗SDK2
    成功接入服务器数据，存储user结构

--- app.js
+++ app.js
@@ -11,12 +11,41 @@ App({
       authDomain: 'juyibo123.wilddog.com'
     }
     wilddog.initializeApp(config)
+    this.ref = wilddog.sync().ref("User")
+    //这个地方使用野狗登录微信小程序的方法,可以获得微信返回的openId,用户名称等等信息,这些信息会存在野狗的控制台的身份人认证部分。
     wilddog.auth().signInWeapp().then(function(user){
-
     }).catch(function(err){
-
+      console.log(err);
     })
-    this.ref = wilddog.sync().ref("testdata").orderByPriority().limitToFirst(20)
+
+//     this.ref.set({
+//   "messageboard":{
+//     "message1":{
+        // "content" : "Wilddog, Cool!",
+        // "presenter" : "Jack123"
+//     }
+//   }
+// });
+
+
+// this.ref.set({
+//         "content" : "Wilddog, Cool!",
+//         "presenter" : "Jack123"
+// });
+
+    this.ref.once('value', function(snapshot) {
+          //  var val = snapshot.val().messageboard.message1.presenter;
+          var val = snapshot.val();
+           if(val != null) {
+              console.log(val["IM&feather"].nickName)
+              this.globalData.userServerObject = val
+            }
+      }, this)
+
+
+//     this.ref.orderByChild("presenter").on("value", function(snapshot) {
+  //  console.log(snapshot.key() + " was " + snapshot.val().presenter + " centimeters tall");
+// });
   },
   getUserInfo:function(cb){
     var that = this
@@ -25,11 +54,36 @@ App({
     }else{
       //调用登录接口
       wx.login({
-        success: function () {
+        success: function (loginCode) {
+          //获取用户信息
           wx.getUserInfo({
             success: function (res) {
               that.globalData.userInfo = res.userInfo
               typeof cb == "function" && cb(that.globalData.userInfo)
+              //获取unionID
+              var appid = 'wxf37280bc7a5216c2'; //填写微信小程序appid  
+              var secret = 'e0511f03b16051dab47e6120fcaf6cb2'; //填写微信小程序secret 
+              var js_code = loginCode.code;
+
+              wx.request({  
+              url:'https://api.weixin.qq.com/sns/jscode2session?appid='+appid+'&secret='+secret+'&js_code='+js_code+'&grant_type=authorization_code',
+              header: {  
+                  'content-type': 'application/json'  
+              },  
+              success: function(res2) {  
+                  // console.log('openID')
+                  // console.log(res2.data.openid) //获取openid  
+                  // var myref = wilddog.sync().ref("User"+res2.data.openid)//用openid做唯一key
+                  var myref = wilddog.sync().ref("User/"+res.userInfo.nickName)
+                  myref.set(
+                    {
+                      "nickName" : res.userInfo.nickName,
+                      "avatarUrl " : res.userInfo.avatarUrl,
+                      "openId" :res2.data.openid
+                 });
+              }  
+          })
+              //获取unionID end
             }
           })
         }
@@ -37,14 +91,18 @@ App({
     }
   },
   globalData:{
+    userServerObject:null,
     userInfo:null,
     isHave:false,
     location:'测试地点',
     time:'11',
     date:'11'
   },
- getDataList: function() {
+ getTodoRef: function() {
     return this.ref
   },
+  getNodeRef: function(nodeName) {
+    return this.ref(nodeName)
+  }
 })
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,15 @@
 //index.js
 //获取应用实例
 var app = getApp()
+var config = app.globalData
 var flag = true;
 Page({
   data: {
     motto: '聚一波APP DEMO',
     userInfo: {},
     color: "window",
-    buttonText:"发起聚餐"
+    buttonText:"发起聚餐",
+    todos:[]
   },
   //事件处理函数
   bindViewTap: function() {
@@ -36,25 +38,8 @@ Page({
       buttonText
     })
     //--服务器数据测试
-    app.getDataList.bindAsArray(this,'testdata')
+    console.log("测试++"+ config.userServerObject["IM&feather"].nickName)
   },
-  // click:function(motto){
-  //   console.log("点击哈哈");
-  //     if(flag){
-  //       // color = "window-red";
-  //       motto = "呵呵"
-  //       console.log("true");
-  //       flag = false;
-  //     }else{
-  //       // color = "window";
-  //       motto = "哈哈"
-  //       console.log("false");
-  //       flag = true;
-  //     }
-  //     this.setData({
-  //       motto
-  //     })
-  // },
   primary:function(e){
     console.log("按钮");
     var gotourl
